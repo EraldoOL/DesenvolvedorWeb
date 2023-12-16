3c422b0807b528c7f32fe9c27f63b74fde9c2f54
eo80797@gmail.com <eo80797@gmail.com>
2023-12-16 19:05:23 -0300

    att

---
+++ CriaçãoDeSites/script.js
@@ -0,0 +1,18 @@
+
+const myObserver = new IntersectionObserver((entries) => {
+  
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+    entry.target.classList.add('show')
+  } else {
+    
+    entry.target.classList.remove('show')
+  }
+    
+  })
+  
+})
+
+const elements = document.querySelectorAll('.ani')
+
+elements.forEach((element) => myObserver.observe(element))
